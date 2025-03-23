c2e888470e5cb3268a739b1d7ca2feaf5a0b9d54
Thanasis-pap <thanasis.papias@gmail.com>
2025-03-23 17:48:45 +0200

    Initialize

---
+++ build/CMakeFiles/test_int24.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_int24.dir/src/int24.c.o"
+  "CMakeFiles/test_int24.dir/src/int24.c.o.d"
+  "CMakeFiles/test_int24.dir/test/test_int24.c.o"
+  "CMakeFiles/test_int24.dir/test/test_int24.c.o.d"
+  "test_int24"
+  "test_int24.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test_int24.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/test_int24.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for test_int24.
+# This may be replaced when dependencies are built.
